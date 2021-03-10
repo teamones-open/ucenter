0b634422b36de8ddad0a92fbaf45438f69ddee23
coder_wen <xianghua_we@163.com>
2021-03-10 18:31:56 +0800

    add: 查询租户用户 tenant_user/select_tenant_user

--- src/service/TenantUser.php
+++ src/service/TenantUser.php
@@ -6,4 +6,26 @@ namespace teamones\ucenter\service;
 
 class TenantUser extends Base
 {
+    const ROUTE_SELECT_TENANT_USER = 'select_tenant_user';
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    private function getSelectTenantUserUrl(): string
+    {
+        return "{$this->getControllerName()}/" . self::ROUTE_SELECT_TENANT_USER;
+    }
+
+    /**
+     * 查询租户用户
+     * @param $data
+     * @param array $headers
+     * @return array|string
+     * @throws \Exception
+     */
+    public function selectTenantUser($data, $headers = [])
+    {
+        return $this->request('POST', $this->getSelectTenantUserUrl(), $data, $headers);
+    }
 }
